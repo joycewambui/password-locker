6cc3391179c10034a1462583cd9e656333d60c1c
joycewambui <jwambui@protonmail.com>
2018-04-02 01:20:16 +0300

    Create username_test.py file

--- usernames.py
+++ usernames.py
@@ -1,4 +1,14 @@
 class Username:
+    '''
+    Class that generates new instances of username information 
+    '''
     def __init__ (self,name,password):
         self.name = name 
         self.password = password
+
+    username_credentials = []
+
+    
+        
+
+
---
+++ usernames_test.py
@@ -0,0 +1,11 @@
+import unittest  # Importing the unittest module
+
+from usernames import Username
+
+
+class TestContact(unittest.TestCase):
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        
