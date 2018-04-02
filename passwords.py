9eb2e2038aa0882173291ed33219a8d860cfc105
joycewambui <jwambui@protonmail.com>
2018-04-02 05:34:19 +0300

    Rewrite passwords.py

--- passwords.py
+++ passwords.py
@@ -1,16 +1,7 @@
-import string
-import random
 
 
-def random_password(size=8, chars=string.ascii_letters + string.digits):
-    """
-    Returns a string of random characters, used in generating random passwords
-    
-    """
-    p = ''.join(random.choice(chars) for i in range(size))
 
-print( "p" )
-    
+
 
         
 
