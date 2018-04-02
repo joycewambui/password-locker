e2324afaaafacbee65c660e99f89a447e9e261d4
joycewambui <jwambui@protonmail.com>
2018-04-02 05:37:52 +0300

    Rewrite passwords.py,generated random passwords

--- passwords.py
+++ passwords.py
@@ -1,5 +1,11 @@
 
+import random
 
+password = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+length = 10
+p = "".join(random.sample(password, length))
+
+print(p)
 
 
 
